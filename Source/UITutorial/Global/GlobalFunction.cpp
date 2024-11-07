3aa90d5d248354805461ccdfe475833436bbd954
kykim9050 <kykim9050@naver.com>
2024-11-07 17:50:44 +0900

    김경윤 : 전역 함수 전용 클래스인 GlobalFunction 클래스 추가

---
+++ Source/UITutorial/Global/GlobalFunction.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/GlobalFunction.h"
+
+#include "Global/MainGameInstance.h"
+
+UMainGameInstance* UGlobalFunction::GetMainGameInstance(const UWorld* WorldContextObject)
+{
+	UMainGameInstance* MainGameInst = WorldContextObject->GetGameInstanceChecked<UMainGameInstance>();
+
+	if (false == MainGameInst->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Fatal, TEXT("%S(%u)> if (false == Inst->IsValidLowLevel())"), __FUNCTION__, __LINE__);
+		return nullptr;
+	}
+
+	return MainGameInst;
+}
---
+++ Source/UITutorial/Global/GlobalFunction.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GlobalFunction.generated.h"
+
+/**
+ * 
+ */
+class UMainGameInstance;
+UCLASS()
+class UITUTORIAL_API UGlobalFunction : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	/// <summary>
+	/// MainGameInstance를 가져오는 static 전역 함수
+	/// </summary>
+	/// <param name="WorldContextObject"></param>
+	/// <returns></returns>
+	static UMainGameInstance* GetMainGameInstance(const UWorld* WorldContextObject);
+
+protected:
+
+private:
+
+};
