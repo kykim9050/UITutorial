a92fea14ffc1f2600f7b7ff3ef6f0c49fe371561
kykim9050 <kykim9050@naver.com>
2025-02-08 10:18:28 +0900

    김경윤 : AddChildren 함수 구현 (필요한 양만큼 ResourceWidget생성 예정)

--- Source/UITutorial/TextureAndMaterialTest/TextureAndMaterialMainWidget.cpp
+++ Source/UITutorial/TextureAndMaterialTest/TextureAndMaterialMainWidget.cpp
@@ -2,4 +2,25 @@
 
 
 #include "TextureAndMaterialTest/TextureAndMaterialMainWidget.h"
+#include "Components/WrapBox.h"
 
+void UTextureAndMaterialMainWidget::AddChildren(int32 childrenNum)
+{
+	if (nullptr == mResourceWidgetClass)
+	{
+		UE_LOG(LogType, Error, TEXT("if (nullptr == ResourceWidgetClass)"));
+		return;
+	}
+
+	for (int32 i = 0; i < childrenNum; i++)
+	{
+		addChild();
+	}
+}
+
+void UTextureAndMaterialMainWidget::addChild()
+{
+	UUserWidget* ChildWidget = CreateWidget<UUserWidget>(this, mResourceWidgetClass);
+
+	mResourceContainer->AddChild(ChildWidget);
+}
--- Source/UITutorial/TextureAndMaterialTest/TextureAndMaterialMainWidget.h
+++ Source/UITutorial/TextureAndMaterialTest/TextureAndMaterialMainWidget.h
@@ -9,9 +9,30 @@
 /**
  * 
  */
+class UWrapBox;
 UCLASS()
 class UITUTORIAL_API UTextureAndMaterialMainWidget : public UBaseUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void AddChildren(int32 childrenNum);
+
+protected:
+
+private:
+	void addChild();
+
+protected:
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> mResourceWidgetClass = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget), meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UWrapBox> mResourceContainer = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	int32 mChildrenNum = 0;
 };
